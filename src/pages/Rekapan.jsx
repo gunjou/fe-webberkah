02761e7e4bd3e479de5b845b45f0f43afb86606e
glgibran7 <gibrangl1167@gmail.com>
2025-04-03 17:22:30 +0800

    update tabel rekapan

--- src/pages/Rekapan.jsx
+++ src/pages/Rekapan.jsx
@@ -5,31 +5,64 @@ import React from "react";
 import { Dropdown, DropdownDivider, DropdownItem } from "flowbite-react";
 import { IoIosArrowDown } from "react-icons/io";
 
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import Table1 from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(nama, tanggal, jamKerStd, jmlhJamKer, selisihJamKer) {
+  return {
+    nama,
+    tanggal,
+    jamKerStd,
+    jmlhJamKer,
+    selisihJamKer,
+  };
+}
+
+const rows = [
+  createData("Septian Wahyu", "v", "3240", "3240", "200"),
+  createData("Guguun Joefandi", "v", "3240", "3240", "200"),
+  createData("Gibran", "v", "3240", "3240", "200"),
+];
+
 const Rekapan = () => {
   return (
     <div className="Rekapan">
+      {/* Navbar Sction */}
       {/* <NavMenu /> */}
 
       <div className="flex">
+        {/* Sidebar Section */}
         {/* <SideMenu /> */}
 
-        {/* component rekapan */}
-        <div className="m-4">
-          <h1 className="text-2xl font-bold">Rekapan Absensi</h1>
-          <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 dark:bg-gray-900">
-            <div className="flex">
+        {/* Table List Pegawai */}
+        <div className="w-full h-full">
+          <div className="title flex text-2xl pt-4 pl-4 font-bold">
+            Rekapan Absensi
+          </div>
+          <div className="tabel rounded-[20px] mt-4 mr-4 ml-4 px-2 shadow-md bg-white w-full h-full">
+            {/* Search box */}
+            <div className="ml-2 mb-6 pt-4 flex items-center justify-between">
+              <span className="flex text-lg pl-2 font-semibold mr-[300px]">
+                Tabel Rekapan
+              </span>
               {/* <!-- Dropdown menu --> */}
-              <div className="flex md:order-2">
+              <div className="flex">
                 <Dropdown
                   arrowIcon={false}
                   inline
                   label={
                     <button
                       type="button"
-                      className="flex mt-5 ml-2 focus:outline-none bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                      className="flex p-3 mt-3.5 ml-2 focus:outline-none bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 focus:ring-4 focus:ring-green-300 font-medium rounded-[20px] text-sm px-3 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                     >
                       Jenis Pegawai
-                      <span className="text-lg pl-2 mt-0.5">
+                      <span className="text-sm pl-2 mt-0.5">
                         <IoIosArrowDown />
                       </span>
                     </button>
@@ -43,100 +76,119 @@ const Rekapan = () => {
                 </Dropdown>
               </div>
               {/* <!-- end Dropdown menu --> */}
-
-              {/* Search box */}
-              <div className="absolute right-5 mt-5">
-                <label for="table-search" class="sr-only">
-                  Search
-                </label>
-                <div class="relative">
-                  <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg
-                      class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                      />
-                    </svg>
-                  </div>
-                  <input
-                    type="text"
-                    id="table-search-users"
-                    class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Search for users"
-                  />
+              <div className="relative">
+                <input
+                  type="text"
+                  id="table-search-users"
+                  className="block p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-[20px] w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search"
+                />
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
                 </div>
               </div>
-              {/* end search box */}
             </div>
-            {/* table start */}
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead class="text-xs text-black bg-[#000000] text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" class="px-6 py-3">
-                    Nama
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Tanggal
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Jam kerja standar
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Jumlah jam kerja
-                  </th>
-                  <th scope="col" class="px-6 py-3">
-                    Selisih jam kerja
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
+            {/* end search box */}
+            <div className="overflow-x-auto pl-2">
+              <div className="bg-white rounded-lg shadow-md mr-2">
+                <TableContainer component={Paper}>
+                  <Table1 sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead className="bg-[#e8ebea]">
+                      <TableRow>
+                        <TableCell className="rounded-l-lg text-black font-bold">
+                          Nama Pegawai
+                        </TableCell>
+                        <TableCell
+                          align="left"
+                          className="text-black font-bold"
+                        >
+                          Tanggal
+                        </TableCell>
+                        <TableCell
+                          align="left"
+                          className="text-black font-bold"
+                        >
+                          Jam Kerja Standar
+                        </TableCell>
+                        <TableCell
+                          align="left"
+                          className="text-black font-bold"
+                        >
+                          Jumlah Jam Kerja
+                        </TableCell>
+                        <TableCell
+                          align="left"
+                          className="rounded-r-lg text-black font-bold"
+                        >
+                          Selisih Jam Kerja
+                        </TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody className="text-red">
+                      {rows.map((row) => (
+                        <TableRow
+                          key={row.nama}
+                          sx={{
+                            "&:last-child td, &:last-child th": { border: 0 },
+                          }}
+                        >
+                          <TableCell component="th" scope="row">
+                            {row.nama}
+                          </TableCell>
+                          <TableCell align="left">{row.tanggal}</TableCell>
+                          <TableCell align="left">{row.jamKerStd}</TableCell>
+                          <TableCell align="left">{row.jmlhJamKer}</TableCell>
+                          <TableCell align="left">
+                            {row.selisihJamKer}
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table1>
+                </TableContainer>
+              </div>
+              {/* Tombol Next dan Prev dengan keterangan halaman */}
+              <div className="flex justify-between items-center mt-4 px-4">
+                {/* Keterangan Halaman */}
+                <span className="text-sm text-gray-500">
+                  Showing 1-12 of 100
+                </span>
+
+                {/* Tombol Next dan Prev */}
+                <div className="flex items-center pb-3 px-4">
+                  <button
+                    type="button"
+                    className="bg-gray-300 text-gray-700 hover:bg-gray-400 focus:ring-4 focus:ring-gray-200 font-medium rounded-l-[20px] text-xs px-4 py-2 border border-black"
                   >
-                    <div class="">
-                      <div class="text-xs font-semibold">Neil Sims</div>
-                    </div>
-                  </th>
-                  <td class="px-6 py-4 text-xs">v</td>
-                  <td class="px-6 py-4 text-xs">
-                    <div class="flex items-center">3240</div>
-                  </td>
-                  <td class="px-6 py-4 text-xs">3440</td>
-                  <td class="px-6 py-4 text-xs">+200</td>
-                </tr>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    class="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    <GrFormPrevious />
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-gray-300 text-gray-700 hover:bg-gray-400 focus:ring-4 focus:ring-gray-200 font-medium rounded-r-[20px] text-xs px-4 py-2 border border-black"
                   >
-                    <div class="">
-                      <div class="text-xs font-semibold">Bonnie Green</div>
-                    </div>
-                  </th>
-                  <td class="px-6 py-4 text-xs">v</td>
-                  <td class="px-6 py-4 text-xs">
-                    <div class="flex items-center">3240</div>
-                  </td>
-                  <td class="px-6 py-4 text-xs">3440</td>
-                  <td class="px-6 py-4 text-xs">+200</td>
-                </tr>
-              </tbody>
-            </table>
-            {/* end table */}
+                    <GrFormNext />
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* end add data section */}
           </div>
         </div>
-        {/* End componen rekapan */}
       </div>
     </div>
   );
